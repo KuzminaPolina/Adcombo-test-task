f03036266c21a0771717e92f889fbcac526fe872
Lina_k <Etein@yandex.ru>
2025-04-05 22:21:46 +0300

    set up timer and swiper

---
+++ vite-project/src/js/main.js
@@ -0,0 +1,26 @@
+import '../style.css'
+import { start_timer } from './timer.js';
+import {Swiper} from "swiper/bundle";
+import "swiper/css/bundle";
+
+start_timer("simple_timer", 28.2);
+
+
+const initMobileOnlySlider = () => {
+  let mobileSlider;
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    mobileSlider = new Swiper (".swiper", {
+      slidesPerView: 1,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+      },
+      pagination: {
+        el: ".swiper-pagination"
+      }
+    })
+  }
+  return mobileSlider;
+}
+
+initMobileOnlySlider();
